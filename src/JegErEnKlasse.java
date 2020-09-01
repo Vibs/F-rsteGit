8adb4c38c50a48fa09e5bf530a9f739c35e6590e
RRoager <rasmus-roager@hotmail.com>
2020-09-01 15:07:16 +0200

    Endnu en sout

--- src/JegErEnKlasse.java
+++ src/JegErEnKlasse.java
@@ -7,6 +7,7 @@ public static void main(String[] args)
     System.out.println("Hej med dig, Vibe");
     System.out.println("Så siger jeg også hej");
     System.out.println("Sidste fra min side af");
+    System.out.println("Test");
 }
 
 }
